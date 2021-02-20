cd1ff7585e32bb9df848e2bdd0417d6f9c39deea
KarinaKlevzhits <72280071+KarinaKlevzhits@users.noreply.github.com>
2021-02-20 22:34:19 +0300

    lesson_fifth -add class "Horizontal Line"

---
+++ snake/HorizontalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+        public HorizontalLine(int xleft,int xreight,int y,char symbol)
+        {
+            pList = new List<Point>();
+           for(int x=xleft;x!=xreight;x++)
+            {
+                Point p = new Point(x, y, symbol);
+                pList.Add(p);
+            }
+        }
+        public void Drow()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
